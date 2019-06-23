2ccbefd57264e6b616c9026153d79df5d586821d
abner.andrade <abne.andrade@gmail.com>
2019-06-23 11:48:27 -0300

    Commit two

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        workers: []
+    };
+
+    removerWorker = index => {
+        const { workers } = this.state;
+    
+        this.setState({
+            workers: workers.filter((worker, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = worker => {
+        this.setState({workers: [...this.state.workers, worker]});
+    }
+
+    render() {
+        const { workers } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
+                <Table
+                    workersData={workers}
+                    removerWorker={this.removerWorker}
+                />
+                <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
