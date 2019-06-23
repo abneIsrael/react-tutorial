ecf37ff40994651d41b14c0b75d58598ddf1ae71
abner.andrade <abne.andrade@gmail.com>
2019-06-23 12:20:17 -0300

    Updates

---
+++ precache-manifest.6ba66b384d89563380c18790665a003c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34715c6d1500a46c5c421fc2bdff832",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "dc02d5f74b1c53fea3d9",
+    "url": "/react-tutorial/static/css/main.ebea8aa6.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "dc02d5f74b1c53fea3d9",
+    "url": "/react-tutorial/static/js/main.94afd8e2.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.11869dc4ecc3a856451f29f24fca520e.js"
+  "/react-tutorial/precache-manifest.6ba66b384d89563380c18790665a003c.js"
 );
 
 self.addEventListener('message', (event) => {
